527618130c7813927c390928be8afa198ee03fd9
wendeldavid <wendel.przygoda@rdstation.com>
2023-01-04 15:07:14 -0300

    telegram bot for rojao bot replacement

---
+++ index.js
@@ -0,0 +1,72 @@
+// import { createRequire } from 'module';
+// const require = createRequire(import.meta.url);
+
+require('dotenv').config();
+const TelegramBot = require('node-telegram-bot-api');
+const bot = new TelegramBot(process.env.TELEGRAM_TOKEN, { polling: false });
+
+const sendMessage = function(chatId, text) {
+    console.log("[INFO] " + text);
+    const res = bot.sendMessage(chatId, text);
+    return res;
+}
+
+const sleep = function(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+const acende = async function(chatId) {
+    const tsssPromise = sendMessage(chatId, "tsssss");
+
+    const shots = "pra";
+    const boom = "POOOW!";
+
+    tsssPromise.then(async (resolve, reject) => {
+        var joinShots = "";
+        for (var i = 0; i < 11;) {
+            const joinCount = Math.floor(Math.random() * 5);
+    
+            for (var j = 0; j < joinCount; j++) {
+                joinShots += shots + " ";
+                i++;
+            }
+            
+            if (!!joinShots) {
+                const firePromise = sendMessage(chatId, joinShots);                
+                
+            }
+            joinShots = "";
+        }
+    
+        const waitForPromise = sendMessage(chatId, "...")
+        waitForPromise.then(async (resolve, reject) => {
+            console.log("aqui era pra ser o BOOOOMMM!!!!");
+        });
+
+
+    });
+
+    const lastBoom = Math.floor(Math.random() * 10);
+    if (lastBoom !== 0) {
+        const waitLastBoom = Math.floor(Math.random() *3) + 3;
+        await sleep(waitLastBoom * 1000);
+        const boomPromise = sendMessage(chatId, boom);
+    }
+}
+
+const Fuguete = {
+    sendMessage : sendMessage,
+    acende : acende,
+}
+
+exports.handler = (event) => {
+    const body = JSON.parse(event.body);
+
+    Fuguete.acende(body.message.chat.id);
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify('BOOM'),
+    };
+
+    return response;    
+};
---
+++ release.sh
@@ -0,0 +1,9 @@
+# !/bin/bash
+
+rm fuguetebot.zip
+
+# rm -rf node_modules
+
+# npm install --only=prod
+
+zip -q -r fuguetebot.zip *
